93529414c03bfefb00500d57852493b500dadbdd
Matt Mattox <bizofmatt@gmail.com>
2023-03-29 14:17:11 -0700

    pre-save before MUI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './routes/Home';
 
 const App = () => {
-  return <div>App</div>
+  return <div>
+    <Router>
+      <Routes>
+        <Route exact path ='/' Component={Home} />
+        {/* ADD OUR OTHER ROUTES HERE */}
+        {/* ADD OUR OTHER ROUTES HERE */}
+      </Routes>
+    </Router>
+  </div>
 }
 
 export default App;
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className="nav-bar">
+    <h2 className="rate-my-txt">
+  RateMy<span className="multicolor">Bathroom</span> 
+</h2>
+    <h3 className="sign-in">Sign In</h3>
+  </div>
+  )
+}
+
+export default Header
